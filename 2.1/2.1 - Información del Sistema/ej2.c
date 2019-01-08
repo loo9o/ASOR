6f2c4667777241cbda058a05ef09b65eecd1d214
loo9o <loony1990@gmail.com>
2019-01-08 01:07:33 +0100

    Update ej2.c

--- 2.1/2.1 - Información del Sistema/ej2.c
+++ 2.1/2.1 - Información del Sistema/ej2.c
@@ -6,15 +6,22 @@
 #include <sys/utsname.h>
 
 int main(){
-	int una;
 	utsname uts;
 	
-	una = uname(&uts);
-	printf("Name: %s\n", uts.sysname);
-	printf("Nodename: %s\n", uts.nodename);
-	printf("Release: %s\n", uts.release);
-	printf("Version: %s\n", uts.version);
-	printf("Machine: %s\n", uts.machine);
+	if(uname(&uts) == -1){
+		printf("Error: %i - %s\n", errno, strerror(errno));
+	}
+	else{
+		printf("==================================\n");
+		printf("Information about current kernel\n");
+		printf("==================================\n");
+		printf("Name: %s\n", uts.sysname);
+		printf("Nodename: %s\n", uts.nodename);
+		printf("Release: %s\n", uts.release);
+		printf("Version: %s\n", uts.version);
+		printf("Machine: %s\n", uts.machine);
+		printf("Domain Name: %s\n", uts.domainname);
+	}
 	
 		
 	return 0;
